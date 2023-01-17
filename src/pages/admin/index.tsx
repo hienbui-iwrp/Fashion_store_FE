50f72a1f0d89715255dc185bc4a989b9629535ea
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2023-01-18 00:42:50 +0700

    update admin

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,60 +1,19 @@
 import React from 'react'
 import { Layout, Menu } from 'antd'
-import type { MenuProps } from 'antd'
-import {
-  LaptopOutlined,
-  NotificationOutlined,
-  HomeOutlined,
-} from '@ant-design/icons'
 import { memo } from 'react'
-import styles from '@/styles/BranchManagement.module.css'
+import LayoutAdmin from '@/components/Layout/LayoutAdmin/LayoutAdmin'
 
-const { Header, Sider, Content } = Layout
-
-const LayoutAdmin = memo(() => {
-  const items: MenuProps['items'] = [
-    HomeOutlined,
-    LaptopOutlined,
-    NotificationOutlined,
-  ].map((icon, index) => {
-    const key = String(index + 1)
-
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `subnav ${key}`,
-      onClick: () => {
-        console.log(key)
-      },
-
-      children: new Array(4).fill(null).map((_, j) => {
-        const subKey = index * 4 + j + 1
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        }
-      }),
-      style: { color: 'white' },
-    }
-  })
-  return (
-    <Layout>
-      <Sider className='!bg-green h-screen'>
-        <Menu
-          className={styles.sider}
-          mode='inline'
-          defaultSelectedKeys={['1']}
-          items={items}
-        />
-      </Sider>
-      <Layout>
-        <Header>Header</Header>
-        <Content>abc</Content>
-      </Layout>
-    </Layout>
+const BranchManagement = memo(() => {
+  const content = (
+    <div>
+      <h1>abc</h1>
+    </div>
   )
+
+  console.log(content)
+  return <LayoutAdmin content={content} />
 })
 
-LayoutAdmin.displayName = 'Layout Admin'
+BranchManagement.displayName = 'Branch Management'
 
-export default LayoutAdmin
+export default BranchManagement
