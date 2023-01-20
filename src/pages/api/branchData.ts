a1723053f8f9f001ffb9f210ac69debaec61106e
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2023-01-20 09:23:41 +0700

    update

---
+++ src/pages/api/branchData.ts
@@ -0,0 +1,46 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  id: string
+  name: string
+  address: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data[]>
+) {
+  res.status(200).json([
+    {
+      id: '1',
+      name: 'Chi nhánh 1',
+      address: `New York No. Lake Park`,
+    },
+    {
+      id: '2',
+      name: 'Chi nhánh 2',
+      address: ` Lake Park`,
+    },
+    {
+      id: '3',
+      name: 'Chi nhánh 3',
+      address: ` Lake Park`,
+    },
+    {
+      id: '4',
+      name: 'Chi nhánh 4',
+      address: `New York No. Lake Park`,
+    },
+    {
+      id: '5',
+      name: 'Chi nhánh 5',
+      address: `New York No. Lake Park`,
+    },
+    {
+      id: '6',
+      name: 'Chi nhánh 6',
+      address: `New York No. Lake Park`,
+    },
+  ])
+}
