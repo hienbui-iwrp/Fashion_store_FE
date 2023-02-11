971e44f9b5ce26f2b24f924c560421db30661750
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2023-02-11 17:49:04 +0700

    update

---
+++ src/utils/axios/axios.tsx
@@ -0,0 +1,28 @@
+import { HOST_BRANCH_SERVICE } from '@/constants'
+import axios from 'axios'
+
+const apiBranchService = axios.create({
+  baseURL: HOST_BRANCH_SERVICE,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  },
+})
+
+// apiBranchService.defaults.headers.common['Authorization'] = 'Bearer '
+
+// //temporary fix for login error
+apiBranchService.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error) => {
+    console.warn('Error status', error.response?.status)
+    console.log('show notification')
+    console.log(error.response?.data)
+    return Promise.reject(error)
+  }
+)
+
+export { apiBranchService }
