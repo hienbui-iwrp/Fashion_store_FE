05d2e17d30629117eab042320084743bb6e0b26c
PhongxTran <92942665+PhongxTran@users.noreply.github.com>
2023-02-14 01:35:43 +0700

    fix layout for page not found

--- src/components/HomeClient/HomeClient.tsx
+++ src/components/HomeClient/HomeClient.tsx
@@ -25,12 +25,14 @@ export default function HomeClient(props: HomeClientProps) {
           <h3 style={contentStyle}>2</h3>
         </div>
       </Carousel>
-      <FeaturedProducts name='SẢN PHẨM BÁN CHẠY NHẤT' /> 
-      <FeaturedProducts name='SẢN PHẨM MỚI' /> 
-      <FeaturedProducts name='THỜI TRANG NAM' /> 
-      <FeaturedProducts name='THỜI TRANG NỮ' /> 
-      <FeaturedProducts name='THỜI TRANG TRẺ EM' /> 
-      <FeaturedProducts name='PHỤ KIỆN' /> 
+      <div className="px-4">
+        <FeaturedProducts name='SẢN PHẨM BÁN CHẠY NHẤT' />
+        <FeaturedProducts name='SẢN PHẨM MỚI' />
+        <FeaturedProducts name='THỜI TRANG NAM' />
+        <FeaturedProducts name='THỜI TRANG NỮ' />
+        <FeaturedProducts name='THỜI TRANG TRẺ EM' />
+        <FeaturedProducts name='PHỤ KIỆN' />
+      </div>
     </div>
   );
 }
