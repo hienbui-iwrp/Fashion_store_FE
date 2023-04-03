5c868c88b86c062bf2ab7207744605e4bba94fe1
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2023-04-03 14:05:59 +0700

    finish event api

--- src/pages/admin/event/detail.tsx
+++ src/pages/admin/event/detail.tsx
@@ -16,11 +16,13 @@ import {
   Space,
   TimePicker,
   Image,
+  Form,
 } from 'antd'
 import { AddButton, RemoveButton, TableList } from '@/components'
 import {
   EventProps,
   formatDate,
+  FormatDateAndTime,
   formatEventDataXML,
   formatGoodsDataXML,
   formatTime,
@@ -36,8 +38,10 @@ import localeData from 'dayjs/plugin/localeData'
 import weekday from 'dayjs/plugin/weekday'
 import weekOfYear from 'dayjs/plugin/weekOfYear'
 import weekYear from 'dayjs/plugin/weekYear'
-import { getEventDetailBFF, getGoodsBFF } from '@/api'
-import { GoodsAges, GoodsGenders, GoodsTypes } from '@/constants'
+import { getEventDetailBFF, getGoodsBFF, updateEventBff } from '@/api'
+import { GoodsAges, GoodsGenders, GoodsTypes, Routes } from '@/constants'
+import { useDispatch } from 'react-redux'
+import { setNotificationType, setNotificationValue } from '@/redux'
 
 dayjs.extend(customParseFormat)
 dayjs.extend(advancedFormat)
@@ -46,12 +50,25 @@ dayjs.extend(localeData)
 dayjs.extend(weekOfYear)
 dayjs.extend(weekYear)
 
+type EventForm = {
+  name?: string
+  discount?: number
+  startTime?: Date
+  startDate?: Date
+  endTime?: Date
+  endDate?: Date
+  image?: string
+  goods?: string[]
+}
+
 const Detail = () => {
   const [data, setData] = useState<EventProps>()
   const [goodsData, setGoodsData] = useState<GoodsProps[]>([])
   const [allGoodsData, setAllGoodsData] = useState<GoodsProps[]>([])
   const [loading, setLoading] = useState(false)
   const [modalAllGoods, setModalAllGoods] = useState(false)
+  const dispatch = useDispatch()
+  const [formData, setFormData] = useState<EventForm>({})
 
   const router = useRouter()
   const { id } = router.query
@@ -195,7 +212,34 @@ const Detail = () => {
     })
   }
 
-  const onSave = () => {}
+  const onSave = async () => {
+    const event: EventProps = {
+      id: '',
+      name: formData?.name,
+      discount: formData?.discount,
+      startTime: FormatDateAndTime(
+        new Date(formData.startDate ?? data?.startTime ?? ''),
+        new Date(formData.startTime ?? data?.startTime ?? '')
+      ),
+      endTime: FormatDateAndTime(
+        new Date(formData.endDate ?? data?.endTime ?? ''),
+        new Date(formData.endTime ?? data?.endTime ?? '')
+      ),
+      image: '',
+      goods: goodsData.map((goods) => goods.id),
+    }
+
+    await updateEventBff(id, event)
+      .then(async (res: any) => {
+        if (res.StatusCode != 200) throw new Error('FAIL')
+        dispatch(setNotificationValue('Đã thêm nhân viên mới'))
+        router.push(Routes.admin.event)
+      })
+      .catch((error) => {
+        dispatch(setNotificationType('error'))
+        dispatch(setNotificationValue('Có lỗi khi thực hiện'))
+      })
+  }
 
   const getData = async () => {
     await getEventDetailBFF(id).then((res: any) => {
@@ -245,7 +289,9 @@ const Detail = () => {
   }, [id])
 
   useEffect(() => {
-    if (data) getGoodsData()
+    if (data) {
+      getGoodsData()
+    }
   }, [data])
 
   return (
@@ -262,9 +308,19 @@ const Detail = () => {
                   <Input
                     className={styles.adminInputShadow}
                     defaultValue={data.name}
+                    onChange={(e) => {
+                      setFormData({ ...formData, name: e.target.value })
+                    }}
                   />
                 )}
-                {!id && <Input className={styles.adminInputShadow} />}
+                {!id && (
+                  <Input
+                    className={styles.adminInputShadow}
+                    onChange={(e) => {
+                      setFormData({ ...formData, name: e.target.value })
+                    }}
+                  />
+                )}
               </Col>
             </Row>
             <Row className={styles.adminRow}>
@@ -276,9 +332,19 @@ const Detail = () => {
                   <InputNumber
                     className={styles.adminInputShadow}
                     defaultValue={data.discount}
+                    onChange={(e) => {
+                      setFormData({ ...formData, discount: e ?? 0 })
+                    }}
                   />
                 )}
-                {!id && <InputNumber className={styles.adminInputShadow} />}
+                {!id && (
+                  <InputNumber
+                    className={styles.adminInputShadow}
+                    onChange={(e: number | null) => {
+                      setFormData({ ...formData, discount: e ?? 0 })
+                    }}
+                  />
+                )}
               </Col>
             </Row>
             <Row className={styles.adminRow}>
@@ -323,12 +389,18 @@ const Detail = () => {
                         formatDate(data?.startTime),
                         'DD/MM/YYYY'
                       )}
+                      onChange={(e) => {
+                        setFormData({ ...formData, startDate: e?.toDate() })
+                      }}
                     />
                   )}
                   {!id && (
                     <DatePicker
                       className={styles.adminInputShadow}
                       format={'DD/MM/YYYY'}
+                      onChange={(e) => {
+                        setFormData({ ...formData, startDate: e?.toDate() })
+                      }}
                     />
                   )}
 
@@ -340,12 +412,18 @@ const Detail = () => {
                         formatDate(data?.endTime),
                         'DD/MM/YYYY'
                       )}
+                      onChange={(e) => {
+                        setFormData({ ...formData, endDate: e?.toDate() })
+                      }}
                     />
                   )}
                   {!id && (
                     <DatePicker
                       className={styles.adminInputShadow}
                       format={'DD/MM/YYYY'}
+                      onChange={(e) => {
+                        setFormData({ ...formData, endDate: e?.toDate() })
+                      }}
                     />
                   )}
                 </Space>
@@ -362,12 +440,18 @@ const Detail = () => {
                       className={styles.adminInputShadow}
                       format={'HH:mm'}
                       defaultValue={dayjs(formatTime(data?.startTime), 'HH:mm')}
+                      onChange={(e) => {
+                        setFormData({ ...formData, startTime: e?.toDate() })
+                      }}
                     />
                   )}
                   {!id && (
                     <TimePicker
                       className={styles.adminInputShadow}
                       format={'HH:mm'}
+                      onChange={(e) => {
+                        setFormData({ ...formData, startTime: e?.toDate() })
+                      }}
                     />
                   )}
                   {data && (
@@ -375,12 +459,18 @@ const Detail = () => {
                       className={styles.adminInputShadow}
                       format={'HH:mm'}
                       defaultValue={dayjs(formatTime(data?.endTime), 'HH:mm')}
+                      onChange={(e) => {
+                        setFormData({ ...formData, endTime: e?.toDate() })
+                      }}
                     />
                   )}
                   {!id && (
                     <TimePicker
                       className={styles.adminInputShadow}
                       format={'HH:mm'}
+                      onChange={(e) => {
+                        setFormData({ ...formData, endTime: e?.toDate() })
+                      }}
                     />
                   )}
                 </Space>
@@ -408,7 +498,12 @@ const Detail = () => {
             icon={<CloseOutlined />}
             borderRadius={10}
           />
-          <AddButton label='Lưu' icon={<CheckOutlined />} borderRadius={10} />
+          <AddButton
+            label='Lưu'
+            icon={<CheckOutlined />}
+            borderRadius={10}
+            onClick={onSave}
+          />
         </Space>
       </Card>
       <TableList<GoodsProps>
